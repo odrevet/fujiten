9ad80c55dafaf2f34239707ae874794d8133e7c6
odrevet <odrevet@gmail.com>
2022-08-21 21:20:00 +0200

    disable download while alderly downloading

--- lib/widgets/settings/database_settings_widget.dart
+++ lib/widgets/settings/database_settings_widget.dart
@@ -87,57 +87,65 @@ class _DatabaseSettingsWidgetState extends State<DatabaseSettingsWidget> {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             ElevatedButton(
-                              onPressed: () async {
-                                Directory appDocDir = await getApplicationDocumentsDirectory();
-                                String appDocPath = appDocDir.path;
-                                String downloadTo = "$appDocPath/${widget.type}.db";
-                                Dio().download(
-                                    "https://github.com/odrevet/edict_database/releases/latest/download/${widget.type}.zip",
-                                    downloadTo, onReceiveProgress: (received, total) {
-                                  if (total != -1) {
-                                    setState(() => downloadLog =
-                                        ("Downloading... ${(received / total * 100).toStringAsFixed(0)}%"));
-                                  }
-                                }).then((_) async {
-                                  String path = "$appDocPath/${widget.type}.db";
+                              onPressed: downloadLog.isNotEmpty
+                                  ? null
+                                  : () async {
+                                      Directory appDocDir =
+                                          await getApplicationDocumentsDirectory();
+                                      String appDocPath = appDocDir.path;
+                                      String downloadTo = "$appDocPath/${widget.type}.db";
+                                      Dio().download(
+                                          "https://github.com/odrevet/edict_database/releases/latest/download/${widget.type}.zip",
+                                          downloadTo, onReceiveProgress: (received, total) {
+                                        if (total != -1) {
+                                          setState(() => downloadLog =
+                                              ("Downloading... ${(received / total * 100).toStringAsFixed(0)}%"));
+                                        }
+                                      }).then((_) async {
+                                        String path = "$appDocPath/${widget.type}.db";
 
-                                  // Extract zip
-                                  try {
-                                    // Read the Zip file from disk.
-                                    final bytes = File(downloadTo).readAsBytesSync();
+                                        // Extract zip
+                                        try {
+                                          // Read the Zip file from disk.
+                                          final bytes = File(downloadTo).readAsBytesSync();
 
-                                    // Decode the Zip file
-                                    final archive = ZipDecoder().decodeBytes(bytes);
+                                          // Decode the Zip file
+                                          final archive = ZipDecoder().decodeBytes(bytes);
 
-                                    // Extract the contents of the Zip archive to disk.
-                                    for (final file in archive) {
-                                      final data = file.content as List<int>;
-                                      File('$appDocPath/${widget.type}.db')
-                                        ..createSync(recursive: true)
-                                        ..writeAsBytesSync(data);
-                                    }
+                                          // Extract the contents of the Zip archive to disk.
+                                          for (final file in archive) {
+                                            final data = file.content as List<int>;
+                                            File('$appDocPath/${widget.type}.db')
+                                              ..createSync(recursive: true)
+                                              ..writeAsBytesSync(data);
+                                          }
 
-                                    // Set DB Path and open the Database
-                                    setPath(path);
-                                    setState(() {
-                                      downloadLog = "";
-                                    });
-                                    await widget.setDb(path);
-                                  } catch (e) {
-                                    setState(() => downloadLog = "Error ${e.toString()}");
-                                  }
-                                });
-                              },
+                                          // Set DB Path and open the Database
+                                          setPath(path);
+                                          setState(() {
+                                            downloadLog = "";
+                                          });
+                                          await widget.setDb(path);
+                                        } catch (e) {
+                                          setState(() => downloadLog = "Error ${e.toString()}");
+                                        }
+                                      });
+                                    },
                               child: const Text('Download'),
                             ),
                             ElevatedButton(
-                              onPressed: () => _pickFiles().then((result) async {
-                                if (result != null) {
-                                  String path = result.first.path!;
-                                  setPath(path);
-                                  await widget.setDb(path);
-                                }
-                              }),
+                              onPressed: downloadLog.isNotEmpty
+                                  ? null
+                                  : () => _pickFiles().then((result) async {
+                                        if (result != null) {
+                                          String path = result.first.path!;
+                                          setPath(path);
+                                          await widget.setDb(path);
+                                          setState(() {
+                                            downloadLog = '';
+                                          });
+                                        }
+                                      }),
                               child: const Text('Pick file'),
                             )
                           ],
