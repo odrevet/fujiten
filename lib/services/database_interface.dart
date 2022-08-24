ba4c3df484b9d6a30229e48537ba544e6d86d4e5
odrevet <odrevet@gmail.com>
2022-08-24 20:58:29 +0200

    refactor

--- lib/services/database_interface.dart
+++ lib/services/database_interface.dart
@@ -9,10 +9,16 @@ abstract class DatabaseInterface {
 
   Future<void> open(String path) async {
     database = sqlite3.open(path, mode: OpenMode.readOnly);
+    database!.createFunction(
+      functionName: 'regexp',
+      function: (args) => RegExp(args[0].toString()).hasMatch(args[1].toString()),
+      argumentCount: const AllowedArgumentCount(2),
+      deterministic: true,
+      directOnly: false,
+    );
   }
 
-  Future<void> dispose() async {
-  }
+  Future<void> dispose() async {}
 
   Future<List<Entry>> search(String input, [resultsPerPage = 10, currentPage = 0]);
 }
